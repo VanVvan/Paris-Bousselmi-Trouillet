c8dd8121e8da0d6c65e8280dbe05f0da3a524cbe
VanVvan <124789548+VanVvan@users.noreply.github.com>
2023-04-02 19:23:44 +0200

    Update Paris-G1-Bousselmi-Trouillet.py

--- Paris-G1-Bousselmi-Trouillet.py
+++ Paris-G1-Bousselmi-Trouillet.py
@@ -5,29 +5,3 @@ Created on Mon Feb 27 16:36:55 2023
 
 @author: evantrouillet
 """
-
-print ("hello from -evan")
-print(" je suis evan ") 
-
-print (" je suis gary")
-
-
-print ("test with branch") 
-
-2+2
-print ("ariane")
-print("ESSCA")
-4+2
-WANDJI
-
-# test
-
-
-#toujours test 
-
-#c'est tjrs un ptn de test 
-
-import seaborn as sns
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as 
